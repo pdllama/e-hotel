b22bc8bf3a33236b877d54dc38a4fe7fd37069de
paranoid-llama <omar_born@hotmail.com>
2026-03-20 00:19:13 -0400

    small changes to seeding

--- src/db/seeding/seeding funcs/room-problem-seeding.ts
+++ src/db/seeding/seeding funcs/room-problem-seeding.ts
@@ -1,8 +1,22 @@
 
+
+
+export interface RoomProblem {
+    problem_id: string,
+    hotel_address: string,
+    room_number: number,
+    type: string,
+    description: string,
+    status: string,
+    log_date: string,
+    resolved_date: string
+}
+
 export function generateRoomProblem(
 
 ) {
     // 5% for a room to have a problem in the first place.
     //  1% chance for this problem to be ongoing
-    //  
+    //  if ongoing - the log date is any time 1-2 days before 
+    //  if not: the resolved date will be any time 1-7 days after its log date
 }
--- src/db/seeding/seedingutils.ts
+++ src/db/seeding/seedingutils.ts
@@ -66,3 +66,71 @@ export function get_rand_idx(length:number) {
 export function get_rand_between(min:number, max:number) {
     return (Math.floor(Math.random() * (max - min + 1)) + min)
 }
+
+const year_min = 2000
+const year_max = 2026
+const monthDays:{[key:number]: number} = 
+{1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
+
+export function get_rand_date() {
+    const year = get_rand_between(year_min, year_max).toString();
+    const monthNum = Math.floor((Math.random()*12)+1);
+    const day = Math.floor(Math.random()*(monthDays[monthNum])+1);
+    return `${year}-${monthNum < 10 ? `0${monthNum}` : monthNum}-${day < 10 ? `0${day}` : day}`
+}
+
+export function get_rand_date_after(date:string, frame:number, min:number=1) {
+    // Returns a rand_date after a given date and (optionally) within a specific time frame (in days)
+    // min refers to the minimum number of days that must pass after the date. usually 1
+    // note date will always be in the format "YYYY-MM-DD"
+
+    let date_year = parseInt(date.slice(0, date.indexOf("-")))
+    let date_month = parseInt(date.slice(date.indexOf("-")+1, date.indexOf("-")+3));
+    let date_day = parseInt(date.slice(date.length-2, date.length));
+
+    const addDays = get_rand_between(min, frame);
+    date_day += addDays 
+    while (date_day > monthDays[date_month%12]) {
+        date_day -= monthDays[date_month%12];
+        date_month += 1
+    } 
+    while (date_month > 12) {
+        date_month -= 12
+        date_year++
+    }
+
+    return `${date_year}-${date_month < 10 ? `0${date_month}` : date_month}-${date_day < 10 ? `0${date_day}` : date_day}`
+}
+
+export function get_rand_date_before(date:string, frame:number, min:number=1) {
+    //same as above but before a date before
+    // Algorithm is a little different, though.
+
+    let date_year = parseInt(date.slice(0, date.indexOf("-")))
+    let date_month = parseInt(date.slice(date.indexOf("-")+1, date.indexOf("-")+3));
+    let date_day = parseInt(date.slice(date.length-2, date.length));
+
+    const removeDays = get_rand_between(min, frame);
+    date_day -= removeDays 
+    while (date_day < 0) {
+        date_month -= 1
+        if (date_month < 1) {
+            date_month = 12;
+            date_year--;
+        }
+        date_day += monthDays[date_month];
+    }
+
+    return `${date_year}-${date_month < 10 ? `0${date_month}` : date_month}-${date_day < 10 ? `0${date_day}` : date_day}`
+}
+
+export function get_rand_time() {
+    const hourNum = get_rand_between(0, 23);
+    const hour = hourNum < 10 ? "0"+hourNum : hourNum.toString()
+    const minNum = get_rand_between(0, 59);
+    const min = minNum < 10 ? "0"+minNum : minNum.toString()
+    const secNum = get_rand_between(0, 59);
+    const sec = secNum < 10 ? "0"+secNum : secNum.toString();
+
+    return `${hour}:${min}:${sec}`
+}
